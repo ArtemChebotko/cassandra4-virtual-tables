52fa3d5f9c7c5b989e0cfb17cec463c8ff806904
Artem Chebotko <artemchebotko@gmail.com>
2022-12-14 22:19:44 -0600

    .

--- wait-python.sh
+++ wait-python.sh
@@ -6,7 +6,7 @@ pip install dse-driver==2.11.1 >/dev/null 2>&1 &
 clear
 
 echo -n 'Installing DSE driver ...'; 
-timeout 60 bash -c 'until pip show dse-driver >/dev/null 2>&1; do sleep 2; echo -n "."; done'; 
+timeout 300 bash -c 'until pip show dse-driver >/dev/null 2>&1; do sleep 2; echo -n "."; done'; 
 sleep 2
 echo ' DONE!'
 echo "DSE driver successfully installed."
