0928d4836560cb8cb560dfb361af72de9c1e01f8
Artem Chebotko <artemchebotko@gmail.com>
2022-12-15 13:53:25 -0600

    Update wait-cqlsh.sh

--- wait-cqlsh.sh
+++ wait-cqlsh.sh
@@ -5,9 +5,9 @@ clear
 echo -n 'Starting a CQL shell ...'; 
 timeout 120 bash -c 'until cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done'; 
 sleep 2
-cqlsh
 echo ' DONE!'
-echo "Cassandra successfully started."
+echo "CQL shell successfully started."
 
 echo ""
 
+cqlsh
